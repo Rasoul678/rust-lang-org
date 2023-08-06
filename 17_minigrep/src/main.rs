630ff0cfa3f9e434755b9449ec5a1b1e253513c4
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2023-08-06 13:04:22 +0330

    :sparkles: grep project

---
+++ 17_minigrep/src/main.rs
@@ -0,0 +1,17 @@
+use std::{env, process};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    // dbg!(args);
+
+    // Get command arguments
+    let config = minigrep::Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
