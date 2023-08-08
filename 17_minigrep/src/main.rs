9ecf57f79c57aaf538c4d7012bc2f351758b1dae
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2023-08-08 18:49:31 +0330

    :recycle: improving I/O project

--- 17_minigrep/src/main.rs
+++ 17_minigrep/src/main.rs
@@ -1,11 +1,11 @@
 use std::{env, process};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
     // dbg!(args);
 
     // Get command arguments
-    let config = minigrep::Config::build(&args).unwrap_or_else(|err| {
+    let config = minigrep::Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
