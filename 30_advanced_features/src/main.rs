e6a58ca7a284ea13dda5998acdbdb273fbd2f2e7
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2023-08-27 17:05:15 +0330

    :sparkles: feat: advanced functions

--- 30_advanced_features/src/main.rs
+++ 30_advanced_features/src/main.rs
@@ -1,7 +1,9 @@
+mod advanced_faunctions;
 mod advanced_traits;
 mod advanced_types;
 mod unsafe_rust;
 
+use advanced_faunctions::advanced_functions;
 use advanced_traits::advanced_traits;
 use advanced_types::advanced_types;
 use unsafe_rust::unsafe_rust;
@@ -10,4 +12,5 @@ fn main() {
     unsafe_rust();
     advanced_traits();
     advanced_types();
+    advanced_functions();
 }
