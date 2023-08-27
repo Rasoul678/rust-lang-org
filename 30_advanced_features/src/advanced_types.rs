877a35311882033051e94d3bb7a32a3018b09941
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2023-08-27 16:38:23 +0330

    :sparkles: feat: advanced types

---
+++ 30_advanced_features/src/advanced_types.rs
@@ -0,0 +1,24 @@
+use std::fmt;
+
+pub fn advanced_types() {
+    type Kilometers = i32;
+
+    let x: i32 = 5;
+    let y: Kilometers = 10;
+
+    println!("x + y = {}", x + y);
+}
+
+// Creating Type Synonyms with Type Aliases
+type Result<T> = std::result::Result<T, std::io::Error>;
+
+pub trait Write {
+    fn write(&mut self, buf: &[u8]) -> Result<usize>;
+    fn flush(&mut self) -> Result<()>;
+
+    fn write_all(&mut self, buf: &[u8]) -> Result<()>;
+    fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<()>;
+}
+
+// The Never Type that Never Returns
+
--- 30_advanced_features/src/main.rs
+++ 30_advanced_features/src/main.rs
@@ -1,10 +1,13 @@
-mod unsafe_rust;
 mod advanced_traits;
+mod advanced_types;
+mod unsafe_rust;
 
-use unsafe_rust::unsafe_rust;
 use advanced_traits::advanced_traits;
+use advanced_types::advanced_types;
+use unsafe_rust::unsafe_rust;
 
 fn main() {
     unsafe_rust();
     advanced_traits();
+    advanced_types();
 }
